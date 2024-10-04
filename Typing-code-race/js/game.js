a474f8d7726be61f200031a13bc335b1d4dc0184
Samkarya <unknown9900person@gmail.com>
2024-10-04 10:05:24 +0530

    Update game.js

--- Typing-code-race/js/game.js
+++ Typing-code-race/js/game.js
@@ -27,7 +27,7 @@ class Game {
         UI.updateGameInfo(0, 100);
     }
     displayCodeWithCursor() {
-        const codeParts = this.currentSnippets.split('');
+        const codeParts = this.currentSnippet.split('');
         this.codeDisplay.innerHTML = '';
         codeParts.forEach((char, index) => {
             const span = document.createElement('span');
